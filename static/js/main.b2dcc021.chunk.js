77c18b037b8dda188323661fab9543595b61b51b
olhaknysh <olhaknysh1994@gmail.com>
2021-03-21 20:47:46 +0200

    Updates

---
+++ static/js/main.b2dcc021.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp-olhaknysh-goit-react-hw-01-components"]=this["webpackJsonp-olhaknysh-goit-react-hw-01-components"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),s=n(3),r=n.n(s),a=n(0),h=function(){return Object(a.jsx)("div",{className:"App"})};r.a.render(Object(a.jsx)(o.a.StrictMode,{children:Object(a.jsx)(h,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.b2dcc021.chunk.js.map
